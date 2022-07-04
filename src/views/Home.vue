903037b0dfe2a68fbd92ccb392222476aaa7c797
Green Wojack <teddy78711@gmail.com>
2022-07-04 09:08:47 +0200

    feature : finished homepage view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,10 +10,19 @@
         </div>
       </div>
     </div>
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss a post. Register for your free account today !</h2>
+        <router-link class="router-button" to="#">
+          Register for FireBlogs <Arrow class="arrow arrow-light"/>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Arrow from "../assets/Icons/arrow-right-light.svg"
 import BlogCard from '../components/BlogCard.vue';
 import BlogPost from '../components/BlogPost.vue'
 export default {
@@ -21,6 +30,7 @@ export default {
   components: {
     BlogPost,
     BlogCard,
+    Arrow,
   },
   data() {
     return {
@@ -75,6 +85,38 @@ export default {
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+  }
+}
+.updates{
+  .container{
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media(min-width: 800px){
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+    .router-button{
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media(min-width: 800px){
+        margin-left: auto;
+      }
+    } 
+    h2{
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media(min-width: 800px){
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
   }
 }
 </style>
